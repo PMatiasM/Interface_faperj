2918cfc590147152c82473cd9c29db4755997270
Pedro Matias Menezes <pedromm2406@gmail.com>
2022-07-20 20:48:21 -0300

    fix: Update API route

--- scripts/preview/components/loadFunctions.js
+++ scripts/preview/components/loadFunctions.js
@@ -1,5 +1,5 @@
 async function loadLists() {
-    const response = await fetch('http://faperj.ddns.net:31415/lists',{ method:"get" })
+    const response = await fetch('http://faperj.ddns.net/api/lists',{ method:"get" })
     const lists = await response.json();
     return lists;
 };
